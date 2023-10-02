0f3d85a425aafc5ed0a8bbaca2b7c49730893bbb
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-10-02 18:09:04 +0100

    vid

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -5,9 +5,9 @@ import Section1 from "../section1/Section1";
 import Section2 from "../section2/Section2";
 import Section3 from "../section3/Section3";
 import Section4 from "../section4/Section4";
-import Section5 from "../section5/Section5";
+
 import Section6 from "../section6/Section6";
-import Section7 from "../section7/Section7";
+
 import Section8 from "../section8/Section8";
 import Carousel from "../Carousel/Carousel";
 import Carro from "../Carousel/Carro";
